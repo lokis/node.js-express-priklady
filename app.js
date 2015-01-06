5738f9f5e54767f6feab100257b868405b8c3fef
Lokis <lokis.git@gmail.com>
2015-01-06 18:23:17 +0100

    modul config sqlite

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
+var config = require('config');
+var db = new sqlite3.Database(config.get('db'));
+
+var access = require('./lib/db-init');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var bookmarks = require('./routes/bookmarks');
 
 var app = express();
 
@@ -24,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/bookmarks', bookmarks);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
