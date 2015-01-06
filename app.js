b65b45f472ef15ff009a1902ce49fdda6129c788
Lokis <lokis.git@gmail.com>
2015-01-06 18:24:30 +0100

    nefacha nak

--- app.js
+++ app.js
@@ -1,71 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-// Database
-var mongo = require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/zkouseniExpress", {native_parser:true});
-
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express'), resource = require('express-resource')
+    , mongoose = require('mongoose');
+var config = require('./config');
 
 var app = express();
 
-// Make our db accessible to our router
-app.use(function(req, res, next){
-    req.db = db;
-    next();
-});
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+//konfigurace a spojeni s databazi
+config.configure(app);
+config.connect(app);
 
 
-module.exports = app;
+//controllery
+var PageController = require('./app/controllers/PageController');
+
+//modely
+var Page = require('./app/models/Page');
+
+//API stranky
+app.resource('pages', PageController, { base: '/api/', load: Page.findOneByUrl });
+
+app.listen(process.env.PORT || 3000);
---
+++ config.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+exports.configure = function (app) {
+    app.configure(function () {
+        //app.use(logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express.static(process.cwd() + '/public'));
+        app.use(app.router);
+    });
+    app.configure('development', function () {
+        app.set('db uri', 'mongodb://localhost:27017/zkouseniExpress');
+    });
+    app.configure('production', function () {
+        app.set('db uri', 'mongodb://user:pass@host:port/dbname');
+    });
+}
+
+exports.connect = function (app) {
+    mongoose.connect(app.get('db uri'), function (err) {
+        if (err) console.log(err);
+    });
+}
